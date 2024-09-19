3753728a242103c01085f3ab09b18718fbfa374d
ArunS-1010 <87597888+ArunS-1010@users.noreply.github.com>
2024-09-19 12:19:12 +0530

    Update AdminSignupPage.js

--- src/pages/AdminSignupPage.js
+++ src/pages/AdminSignupPage.js
@@ -30,7 +30,7 @@ const AdminSignupPage = () => {
     }
 
     try {
-      await axios.post('http://localhost:5000/admin/signup', {
+      await axios.post('https://donationlog-backend.onrender.com/admin/signup', {
         username,
         password,
         email, // Use the email from state
